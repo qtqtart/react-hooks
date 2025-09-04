db409b5d8f5ff14d0bb2b01a775d8789ac600db9
artem <qtqt.art@gmail.com>
2025-09-05 00:24:28 +0300

    feat: add useMutationObserver

---
+++ src/hooks/use-mutation-observer.ts
@@ -0,0 +1,50 @@
+import React from "react";
+
+const DEFAULT_OPTIONS: MutationObserverInit = {
+  attributes: true,
+  childList: true,
+  subtree: true,
+};
+
+type useMutationObserverProps<T extends HTMLElement = HTMLElement> = {
+  targetRef: React.RefObject<T | null>;
+  callback: MutationCallback;
+  options?: MutationObserverInit;
+};
+
+export function useMutationObserver<T extends HTMLElement = HTMLElement>({
+  targetRef,
+  callback,
+  options = DEFAULT_OPTIONS,
+}: useMutationObserverProps<T>) {
+  const observerRef = React.useRef<MutationObserver | null>(null);
+  const prevTargetRef = React.useRef<T | null>(null);
+
+  const disconnect = React.useCallback(() => {
+    if (observerRef.current) {
+      observerRef.current.disconnect();
+      observerRef.current = null;
+    }
+  }, []);
+
+  React.useEffect(() => {
+    if (!targetRef.current) {
+      return;
+    }
+
+    if (targetRef.current !== prevTargetRef.current) {
+      disconnect();
+      prevTargetRef.current = targetRef.current;
+    }
+
+    observerRef.current = new MutationObserver(callback);
+    observerRef.current.observe(targetRef.current, options);
+
+    return () => {
+      disconnect();
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [targetRef, callback, options]);
+
+  return { disconnect };
+}
