697f133a85276eef31ccf5e0f57944c8471b6a62
artem <qtqt.art@gmail.com>
2025-09-06 22:40:05 +0300

    Feat: add useUnmount

---
+++ src/hooks/use-unmount/use-unmount.ts
@@ -0,0 +1,10 @@
+import React, { useEffect } from 'react';
+
+export function useUnmount(callback: () => void) {
+  const ref = React.useRef(callback);
+  ref.current = callback;
+
+  useEffect(() => () => {
+    ref.current();
+  });
+}
